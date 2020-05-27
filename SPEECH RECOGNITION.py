41dba4372324737b35820343f69491a7dd09152f
somnathdash <53251639+somnathdash@users.noreply.github.com>
2020-05-28 02:33:49 +0530

    Add files via upload

---
+++ SPEECH RECOGNITION.py
@@ -0,0 +1,25 @@
+import speech_recognition as sr
+import webbrowser as wb
+
+r1=sr.Recognizer()
+r2=sr.Recognizer()
+r3=sr.Recognizer()
+
+with sr.Microphone() as source:
+    print("speak now")
+    audio=r3.listen(source)
+if 'edureka' in r2.recognize_google(audio):
+    r2=sr.Recognizer()
+    url='https://www.edureka.com/'
+    with sr.Microphone() as source:
+        print("search your query")
+        audio=r2.listen(source)
+
+        try:
+            get=r2.recognize_google(audio)
+            print(get)
+            wb.get().open_new(url+get)
+        except sr.UnknownValueError:
+            print("error")
+        except sr.RequestError as e:
+            print("failed".format(e))
